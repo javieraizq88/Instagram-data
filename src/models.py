f662de6b39ffd58896d3d2f75299efbf663c9453
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-05-06 14:23:55 -0400

    first commit

--- src/models.py
+++ src/models.py
@@ -8,29 +8,26 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = 'user'
+class Person(Base):
+    __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    userName = Column(String(250), nullable=False)
-    password = Column(String(250), nullable=False)
-    email = Column(String(250), nullable=False)
-    followings = Column(String(250), nullable=False)
-    follower_id = Column(Integer, ForeignKey('follower.id'))
-    following_id = Column(Integer, ForeignKey('following.id'))
+    name = Column(String(250), nullable=False)
 
+class Address(Base):
+    __tablename__ = 'address'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    street_name = Column(String(250))
+    street_number = Column(String(250))
+    post_code = Column(String(250), nullable=False)
+    person_id = Column(Integer, ForeignKey('person.id'))
+    person = relationship(Person)
 
-    def serialize(self): # cambiar el objeto python a JSON
-        # son los datos q devuelve de la tabla
-        return {
-            "id": self.id,
-            "userName": self.userName,
-            "email": self.email,
-            "followings": self.followings,
-            "follower_id": self.follower_id,
-            "following_id": self.following_id
-        }
+    def to_dict(self):
+        return {}
 
 ## Draw from SQLAlchemy base
-render_er(Base, 'diagramUML.png')
+render_er(Base, 'diagram.png')
